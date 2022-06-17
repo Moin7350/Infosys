4c20337afcf4e38b511c6af432057b953fe00422
unknown <info14java140@iiht.tech>
2022-06-17 15:53:38 +0530

    Moin Commited

---
+++ src/main/java/com/infosys/UberServiceApplication.java
@@ -0,0 +1,14 @@
+package com.infosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberServiceApplication.class, args);
+		System.out.println("Servive Done By Kumar");
+	}
+
+}
